b8635def74128f9d8bf1a12bc60931d7fd232464
itchyny <itchyny@hatena.ne.jp>
2017-10-01 11:10:39 +0900

    add tests

---
+++ tests/lib.rs
@@ -0,0 +1,27 @@
+extern crate mackerel_plugin;
+extern crate mackerel_plugin_loadavg;
+
+use mackerel_plugin::Plugin;
+use mackerel_plugin_loadavg::LoadavgPlugin;
+
+use std::io::Cursor;
+
+#[test]
+fn plugin_output_values() {
+    let plugin = LoadavgPlugin {};
+    let mut out = Cursor::new(Vec::new());
+    assert_eq!(plugin.output_values(&mut out).is_ok(), true);
+    let out_str = String::from_utf8(out.into_inner()).unwrap();
+    assert_eq!(out_str.contains("loadavg1\t"), true);
+    assert_eq!(out_str.contains("loadavg5\t"), true);
+    assert_eq!(out_str.contains("loadavg15\t"), true);
+}
+
+#[test]
+fn plugin_output_definitions() {
+    let plugin = LoadavgPlugin {};
+    let mut out = Cursor::new(Vec::new());
+    assert_eq!(plugin.output_definitions(&mut out).is_ok(), true);
+    let out_str = String::from_utf8(out.into_inner()).unwrap();
+    assert_eq!(out_str.starts_with("# mackerel-agent-plugin\n"), true);
+}
